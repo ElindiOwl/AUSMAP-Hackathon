967b68bc46d22218e2832fe5dc43eb8d166abdbe
elindi <prostonaf@gmail.com>
2025-08-24 11:12:40 +1000

    v7.2

--- src/root.tsx
+++ src/root.tsx
@@ -18,6 +18,15 @@ export const links: Route.LinksFunction = () => [
 		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
 		rel: 'stylesheet',
 	},
+	{
+		href: '/AUSMAPICON.png',
+		rel: 'icon',
+		type: 'image/png',
+	},
+	{
+		href: '/AUSMAPICON.png',
+		rel: 'apple-touch-icon',
+	},
 ];
 
 // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
